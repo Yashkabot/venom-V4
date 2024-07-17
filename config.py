96a2414435794ed99cebeeb57581339ec1cbd52d
Yashkabot <yashkabot815@gmail.com>
2024-07-17 17:11:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7429405959:AAGRc4jKNUARizJDO3vQ27oLGNRTjL_VA2o")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6554029030")
 
 
 
